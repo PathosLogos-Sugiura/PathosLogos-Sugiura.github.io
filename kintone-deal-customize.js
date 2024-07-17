eeaecdd7ff62606ce6b6144b86409f7cac536a55
PathosLogos-Sugiura <hidekazu.sugiura@pathoslogos.co.jp>
2024-07-17 20:21:49 +0900

    Update

--- kintone-deal-customize.js
+++ kintone-deal-customize.js
@@ -4,6 +4,10 @@
     const APP_ID = 21;
 
     const INVOICE_TIMING_BULK_INITIAL = "開始月一括";
+    const PRODUCT_SUPPLIER_OWN = "パトスロゴス";
+    const PRODUCT_SUPPLIER_PARTNER = "共創パートナー";
+    const PRODUCT_TYPE_INITIAL = "初期";
+    const PRODUCT_TYPE_MONTHLY = "月額";
 
     kintone.events.on(['app.record.detail.show'], function (event) {
         const menuButton = document.createElement('button');
@@ -25,46 +29,8 @@
         }
         // 請求データの作成
         createInvoice(event);
-        var customer_id = record.deliver_to_number.value;
-        var customer_name = record.CUSTOMER_NAME.value;
-        var deal_name = record.DEAL_NAME.value;
-        var monthly_amount = record.MONTHLY_AMOUNT.value;
-        var contract_months = record.CONTRACT_MONTHS.value;
-        var discount_momths = record.DISCOUNT_MONTHS.value;
-        var start_date = record.START_DATE.value;
-        var end_date;
-        if (dayjs(start_date).date() == 1) {
-            end_date = dayjs(start_date).add(contract_months, 'month').subtract(1, 'month').endOf('month');
-        } else {
-            end_date = dayjs(start_date).add(contract_months, 'month').endOf('month');
-        }
-        event.record.END_DATE.value = end_date.format('YYYY-MM-DD');
-        alert(end_date);
-        console.log(end_date);
-        var total_amount = monthly_amount * contract_months;
-        var charge_months = contract_months - discount_momths;
-        var charge_monthly_amount = total_amount / charge_months;
-        for (let i = 1; i <= charge_months; i++) {
-            // 新しいレコードのデータを準備
-            var newData = {
-                "app": APP_ID, // 別のアプリのアプリIDを指定
-                "record": {
-                    // 別のアプリにコピーしたいフィールドに対応する値を設定
-                    "CUSTOMER_ID": { "value": customer_id },
-                    "DEAL_NAME": { "value": deal_name },
-                    "INDEX": { "value": i },
-                    "AMOUNT": { "value": charge_monthly_amount },
-                }
-            };
-            // Kintone APIを使って新しいレコードを作成
-            kintone.api(kintone.api.url('/k/v1/record', true), 'POST', newData, function (resp) {
-                // 成功した場合の処理
-                console.log("新しいレコードが作成されました:", resp);
-            }, function (error) {
-                // エラーが発生した場合の処理
-                console.error("エラーが発生しました:", error);
-            });
-        }
+        // 売上データの作成
+        createRevenue(event);
         return event;
     });
 
@@ -74,11 +40,127 @@
         charge_type;
         partner_name;
         product_name;
-        sales_amount;
-        finance_amount;
+        amount_for_sales;
+        amount_for_finance;
 
         constructor(date) {
             this.date = date;
+        }
+    }
+
+    class DealDetails {
+        charge_type;
+        details_remarks;
+        initial_amount;
+        list_price_purchase;
+        list_price_to_customer;
+        month_count;
+        monthly_amount;
+        monthly_period_amount;
+        option_name;
+        own_initial_amount;
+        own_monthly_amount;
+        own_monthly_period_amount;
+        partner_initial_amount;
+        partner_initial_purchase_amount;
+        partner_monthly_amount;
+        partner_monthly_period_amount;
+        partner_monthly_purchase_amount;
+        partner_name;
+        product_name;
+        product_number;
+        product_supplier;
+        product_type;
+        purchase_amount;
+        qty;
+        quotation_product_name;
+        quote_amount;
+        constructor(value) {
+            this.charge_type = value.charge_type.value;
+            this.details_remarks = value.details_remarks.value;
+            this.initial_amount = value.initial_amount.value;
+            this.list_price_purchase = value.list_price_purchase.value;
+            this.list_price_to_customer = value.list_price_to_customer.value;
+            this.month_count = value.month_count.value;
+            this.monthly_amount = value.monthly_amount.value;
+            this.monthly_period_amount = value.monthly_period_amount.value;
+            this.option_name = value.option_name.value;
+            this.own_initial_amount = value.own_initial_amount.value;
+            this.own_monthly_amount = value.own_monthly_amount.value;
+            this.own_monthly_period_amount = value.own_monthly_period_amount.value;
+            this.partner_initial_amount = value.partner_initial_amount.value;
+            this.partner_initial_purchase_amount = value.partner_initial_purchase_amount.value;
+            this.partner_monthly_amount = value.partner_monthly_amount.value;
+            this.partner_monthly_period_amount = value.partner_monthly_period_amount.value;
+            this.partner_monthly_purchase_amount = value.partner_monthly_purchase_amount.value;
+            this.partner_name = value.partner_name.value;
+            this.product_name = value.product_name.value;
+            this.product_number = value.product_number.value;
+            this.product_supplier = value.product_supplier.value;
+            this.product_type = value.product_type.value;
+            this.purchase_amount = value.purchase_amount.value;
+            this.qty = value.qty.value;
+            this.quotation_product_name = value.quotation_product_name.value;
+            this.quote_amount = value.quote_amount.value;
+        }
+    }
+
+    class DealDetailGroup {
+        start_date;
+        end_date;
+        deal_details = [];
+        constructor() {
+        }
+
+        month_duration_for_finance() {
+            var start_month_ratio = 1.0;
+            var start_month_date = dayjs(this.start_date).date();
+            var is_start_month_split = false;
+            if (start_month_date != 1) {
+                is_start_month_split = true;
+                var month_days = getEndOfMonth(this.start_date).date();
+                start_month_ratio = Math.round((month_days - start_month_date + 1) / month_days * 100) / 100;
+            }
+            var end_month_ratio = 1.0;
+            var end_month_date = dayjs(this.end_date).date();
+            var end_month_end_date = getEndOfMonth(this.end_date).date();
+            var is_end_month_split = false;
+            if (end_month_date != end_month_end_date) {
+                is_end_month_split = true;
+                end_month_ratio = Math.round(end_month_date / end_month_end_date * 100) / 100;
+            }
+        }
+
+        initial_amount_sum() {
+            var sum = 0;
+            for (deal_detail of deal_details) {
+                sum = sum + deal_detail.initial_amount;
+            }
+            return sum;
+        }
+
+        initial_purchase_amount_sum() {
+            var sum = 0;
+            for (deal_detail of deal_details) {
+                sum = sum + deal_detail.partner_initial_purchase_amount;
+            }
+            return sum;
+        }
+
+        monthly_amount_sum() {
+            var sum = 0;
+            for (deal_detail of deal_details) {
+                sum = sum + deal_detail.monthly_amount;
+            }
+            return sum;
+        }
+
+        monthly_purchase_amount_sum() {
+            var sum = 0;
+            for (deal_detail of deal_details) {
+                sum = sum + deal_detail.partner_monthly_purchase_amount;
+            }
+            return sum;
         }
     }
 
@@ -108,6 +190,7 @@
         grand_total_amount;
         consumption_tax;
         grand_total_amount_with_tax;
+        deal_details = [];
         constructor(record) {
             // 案件レコード番号
             this.deal_number = record.レコード番号.value;
@@ -120,7 +203,7 @@
             if (this.invoice_to_number != this.deliver_to_number) {
                 this.invoice_item_suffix = "(" + this.deliver_to_name + "様利用分)";
             }
-                // パトスロゴス初期費用
+            // パトスロゴス初期費用
             this.own_initial_invoice_timing = record.own_initial_invoice_timing.value;
             this.own_initial_total_amount = record.own_initial_total_amount.value;
             this.own_initial_start_date = record.own_initial_start_date.value;
@@ -144,11 +227,49 @@
             this.grand_total_amount = record.grand_total_amount.value;
             this.consumption_tax = record.consumption_tax.value;
             this.grand_total_amount_with_tax = record.grand_total_amount_with_tax.value;
+            for (var row_value of record.quotation_details_table.value) {
+                deal_details.push(new DealDetails(row_value));
+            }
+        }
+
+        createDealDetailGroups() {
+            const map = new Map();
+            for (deal_detail of deal_details) {
+                var key = this.createKey(deal_detail);
+                var detail_group = map.get(key);
+                if (detail_group == null) {
+                    detail_group = new DealDetailGroup();
+                    if (deal_detail.product_supplier == PRODUCT_SUPPLIER_OWN && deal_detail.product_type == PRODUCT_TYPE_INITIAL) {
+                        detail_group.start_date = this.own_initial_start_date;
+                        detail_group.end_date = this.own_initial_end_date;
+                    }
+                    if (deal_detail.product_supplier == PRODUCT_SUPPLIER_OWN && deal_detail.product_type == PRODUCT_TYPE_MONTHLY) {
+                        detail_group.start_date = this.own_monthly_start_date;
+                        detail_group.end_date = this.own_monthly_end_date;
+                    }
+                    if (deal_detail.product_supplier == PRODUCT_SUPPLIER_PARTNER && deal_detail.product_type == PRODUCT_TYPE_INITIAL) {
+                        detail_group.start_date = this.partner_initial_start_date;
+                        detail_group.end_date = this.partner_initial_end_date;
+                    }
+                    if (deal_detail.product_supplier == PRODUCT_SUPPLIER_PARTNER && deal_detail.product_type == PRODUCT_TYPE_MONTHLY) {
+                        detail_group.start_date = this.partner_monthly_start_date;
+                        detail_group.end_date = this.partner_monthly_end_date;
+                    }
+                    map.set(key, detail_group);
+                }
+                detail_group.deal_details.push(deal_detail);
+            }
+            return map.values;
+        }
+
+        createKey(deal_detail) {
+            return deal_detail.product_supplier + "_" + deal_detail.product_type + "_" + deal_detail.partner_name + "_" + deal_detail.product_name;
         }
     }
 
     function createRevenue(event) {
         var record = event.record;
+        var deal_info = new DealInfo(record);
 
     }
 
@@ -210,7 +331,7 @@
                         value: deal_info.partner_initial_end_date
                     },
                     'amount': {
-                        value:deal_info. partner_initial_total_amount
+                        value: deal_info.partner_initial_total_amount
                     }
                 }
             };
@@ -252,11 +373,17 @@
             }
         };
         kintone.api(kintone.api.url('/k/v1/record', true), 'POST', newData, function (resp) {
-            console.log("新しいレコードが作成されました:", resp);
+            consoleLog("Created new invoice record.");
         }, function (error) {
             event.error = "請求書登録時にエラーが発生しました";
-            console.error("エラーが発生しました:", error);
+            consoleError("Error occured during invoice registration:", error);
         });
+    }
+
+    // 指定された日付の月末を算出
+    function getEndOfMonth(date) {
+        var end_date = dayjs(date).endOf('month');
+        return end_date;
     }
 
     // 指定された日付の翌月末を算出
@@ -265,17 +392,14 @@
         return end_date;
     }
 
-    // 日付をフォーマットする関数（YYYY-MM-DD形式）
-    function formatDate(date) {
-        var year = date.getFullYear();
-        var month = padZero(date.getMonth() + 1); // 月は0-indexedなので+1する
-        var day = padZero(date.getDate());
-        return year + '-' + month + '-' + day;
+    // コンソールにログ出力（ブラウザで見やすいように接頭語をつけている）
+    function consoleLog(message) {
+        console.log("[PathosLogos] " + message);
     }
 
-    // ゼロパディングする関数
-    function padZero(num) {
-        return ("0" + num).slice(-2);
+    // コンソールにエラー出力（ブラウザで見やすいように接頭語をつけている）
+    function consoleError(message, error) {
+        console.error("[PathosLogos] " + message, error);
     }
 }
 )();
