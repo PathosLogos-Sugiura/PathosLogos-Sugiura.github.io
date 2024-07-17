7a07354bdde4c210a25796c1967c1977ddadf996
PathosLogos-Sugiura <hidekazu.sugiura@pathoslogos.co.jp>
2024-07-17 13:03:09 +0900

    bug fix

--- kintone-deal-customize.js
+++ kintone-deal-customize.js
@@ -160,7 +160,7 @@
             table_value.push(newRow);
         }
         var invoice_issue_date = dayjs();
-        var payment_due_date = getNextEndOfMonth(invoice_date)
+        var payment_due_date = getNextEndOfMonth(invoice_issue_date)
         var newData = {
             "app": INVOICE_APP_ID,
             "record": {
