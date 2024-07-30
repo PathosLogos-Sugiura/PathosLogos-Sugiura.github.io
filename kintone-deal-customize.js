1c77e12dcbbe5daf9b486c0773a3fe8584cd4d3a
PathosLogos-Sugiura <hidekazu.sugiura@pathoslogos.co.jp>
2024-07-30 16:41:26 +0900

    debug

--- kintone-deal-customize.js
+++ kintone-deal-customize.js
@@ -543,6 +543,7 @@
             }
         }
         for (var row_value of record.quotation_details_table.value) {
+            dumpObject(row_value);
             if (row_value.product_supplier.value == PRODUCT_SUPPLIER_PARTNER && isBlank(row_value.purchase_amount.value)) {
                 row_value.purchase_amount.error = '仕入額を入力してください';
             }
