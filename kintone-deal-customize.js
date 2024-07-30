f7f3d6265f376a4e47d3e758f5d705b3af872cc0
PathosLogos-Sugiura <hidekazu.sugiura@pathoslogos.co.jp>
2024-07-30 17:07:27 +0900

    bug fix

--- kintone-deal-customize.js
+++ kintone-deal-customize.js
@@ -543,8 +543,8 @@
             }
         }
         for (var row_value of record.quotation_details_table.value) {
-            if (row_value.product_supplier.value == PRODUCT_SUPPLIER_PARTNER && (row_value.purchase_amount == null || isBlank(row_value.purchase_amount.value))) {
-                row_value.purchase_amount.error = '仕入額を入力してください';
+            if (row_value.value.product_supplier.value == PRODUCT_SUPPLIER_PARTNER && isBlank(row_value.value.purchase_amount.value)) {
+                row_value.value.purchase_amount.error = '仕入額を入力してください';
             }
         }
     }
